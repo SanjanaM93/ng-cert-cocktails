ea0b086b35b98b2a2f172fe4de4476eb1d90c13e
SanjanaM93 <indiainvented0@gmail.com>
2024-07-13 02:06:25 +0530

    "fourth commit"

--- cheers-level-2-structure/src/app/app.routes.ts
+++ cheers-level-2-structure/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CocktailsListComponent } from './cocktails-list/cocktails-list.componen
 import { CocktailComponent } from './cocktail/cocktail.component';
 
 export const routes: Routes = [
-    { path: '', component: CocktailsListComponent},
+    { path:'',redirectTo:'cocktail-list',pathMatch:'full'},
     { path: 'cocktail-list', component: CocktailsListComponent},
     { path:'cocktail-details/:id', component:CocktailComponent},
     { path: '**', redirectTo: 'cocktail-list' }
